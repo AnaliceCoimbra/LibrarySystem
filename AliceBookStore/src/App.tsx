8efe910ad1e2f72e6fa8353bc76b44db09076be6
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-09-20 01:07:44 -0300

    Feature: Initiate Frontend project

---
+++ AliceBookStore/src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header className='flex flex-row justify-between shadow p-4'> 
+    <div> 
+      <h1 className='font-bold text-4xl text-[#3D2B1F]'> Have I ever read it? </h1>
+    </div>
+    <div>       
+      <h3 className='font-medium text-lg mt-3'> What are you reading, now? </h3>
+    </div>
+    <div> 
+      <h3 className='font-medium text-lg mt-3'>Made by Analice Coimbra </h3>
+    </div>
+    </header>
+
+    <main className='bg-[#EFEAD9] m-4 border-2'>
+        <p> test</p>
+    </main>
+
+    <footer className='bg-[#EFEAD9] mb-0 border-2'>
+        <p> test</p>
+    </footer>
+    </>
+  )
+}
+
+export default App
