444d353819a40b3183c0ee8ec27cc205ddc287ff
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-09-19 23:19:17 -0300

    Fix: Adjust indentation in Library Item class

--- API_AliceBookStore/BookStoreBackend/src/main/java/com/analices/bookstorebackend/model/LibraryItem.java
+++ API_AliceBookStore/BookStoreBackend/src/main/java/com/analices/bookstorebackend/model/LibraryItem.java
@@ -5,54 +5,73 @@ public abstract class LibraryItem implements ReadingManageable {
         private String title;
         private String coverUrl;
         private ReadingStatus status;
+        private String genre;
+        private int pages;
 
-        public LibraryItem() {
-            this.status = ReadingStatus.WANT_TO_READ; // The Standard status when created
-        }
+    public LibraryItem() {
+        this.status = ReadingStatus.WANT_TO_READ; // The Standard status when created
+    }
 
-        public LibraryItem(int id, String title, String coverUrl, ReadingStatus status) {
-            this.id = id;
-            this.title = title;
-            this.coverUrl = coverUrl;
-            this.status = status;
-        }
+    public LibraryItem(int id, String title, String coverUrl, ReadingStatus status, String genre, int pages) {
+        this.id = id;
+        this.title = title;
+        this.coverUrl = coverUrl;
+        this.status = status;
+        this.genre = genre;
+        this.pages = pages;
+    }
 
-        public int getId() {
-            return id;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public String getCoverUrl() {
-            return coverUrl;
-        }
+    public String getCoverUrl() {
+        return coverUrl;
+    }
 
-        public void setCoverUrl(String coverUrl) {
-            this.coverUrl = coverUrl;
-        }
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
 
-        public ReadingStatus getStatus() {
-            return status;
-        }
+    public ReadingStatus getStatus() {
+        return status;
+    }
 
-        public void setStatus(ReadingStatus status) {
-            this.status = status;
-        }
+    public void setStatus(ReadingStatus status) {
+        this.status = status;
+    }
+    public String getGenre() {
+        return genre;
+    }
 
-        @Override
-        public void updateStatus(ReadingStatus newStatus) {
-            this.status = newStatus;
-        }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
-        public abstract String getType(); // Identifies if its "BOOK" or "MAGAZINE"
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public void updateStatus(ReadingStatus newStatus) {
+        this.status = newStatus;
+    }
+
+    public abstract String getType(); // Identifies if its "BOOK" or "MAGAZINE"
     }
