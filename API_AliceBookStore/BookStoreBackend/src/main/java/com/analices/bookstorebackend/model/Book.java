65b3cd9a63b86c487e86d8f09f1ce47b956b5a91
Analice Coimbra <analicecoimbra2000@gmail.com>
2026-09-19 22:15:01 -0300

    Add: Library item repository CRUD for Library Item

--- API_AliceBookStore/BookStoreBackend/src/main/java/com/analices/bookstorebackend/model/Book.java
+++ API_AliceBookStore/BookStoreBackend/src/main/java/com/analices/bookstorebackend/model/Book.java
@@ -2,16 +2,14 @@ package com.analices.bookstorebackend.model;
 
 public class Book extends LibraryItem{
     private String author;
-    private int isbn;
 
     public Book() {
         super();
     }
 
-    public Book(int id, String title, String coverUrl, ReadingStatus status, String author, int isbn) {
+    public Book(int id, String title, String coverUrl, ReadingStatus status, String author) {
         super(id, title, coverUrl, status); //To use the parent class attributes
         this.author = author;
-        this.isbn = isbn;
     }
 
     public String getAuthor() {
@@ -20,14 +18,6 @@ public class Book extends LibraryItem{
 
     public void setAuthor(String author) {
         this.author = author;
-    }
-
-    public int getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(int isbn) {
-        this.isbn = isbn;
     }
 
     @Override
---
+++ API_AliceBookStore/BookStoreBackend/src/main/java/com/analices/bookstorebackend/repository/LibraryRepository.java
@@ -0,0 +1,76 @@
+package com.analices.bookstorebackend.repository;
+
+import com.analices.bookstorebackend.model.Book;
+import com.analices.bookstorebackend.model.LibraryItem;
+import com.analices.bookstorebackend.model.Magazine;
+import com.analices.bookstorebackend.model.ReadingStatus;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class LibraryRepository {
+
+    @Repository
+    public class BookRepository {
+
+        //Creating a Catalog List to add books and magazines
+        private final List<LibraryItem> catalog = new ArrayList<>();
+        private int nextId = 0;
+
+        // Save new Item
+        public LibraryItem saveNewItem(LibraryItem item) {
+            item.setId(nextId++);
+            catalog.add(item);
+            return item;
+        }
+
+        // List all catalog
+        public List<LibraryItem> findAll() {
+            return catalog;
+        }
+
+        // Get by ID
+        public Optional<LibraryItem> findById(int id) {
+            return catalog.stream()
+                    .filter(item -> item.getId() == id)
+                    .findFirst();
+        }
+
+        // Filter by status (Reading, Want to Read, Completed)
+        public List<LibraryItem> findByStatus(ReadingStatus status) {
+            return catalog.stream()
+                    .filter(item -> item.getStatus() == status)
+                    .collect(Collectors.toList());
+        }
+
+        // Update Item attributes
+        public LibraryItem updateItemAttributes(int id, LibraryItem updatedItem) {
+            Optional<LibraryItem> existingItemOpt = findById(id);
+            if (existingItemOpt.isEmpty()) {
+                return null;
+            } else {
+                LibraryItem item = existingItemOpt.get();
+                item.setTitle(updatedItem.getTitle());
+                item.setCoverUrl(updatedItem.getCoverUrl());
+                item.setStatus(updatedItem.getStatus());
+
+                //If the item type is a Book, updates the book attributes (author)
+                if (item instanceof Book book && updatedItem instanceof Book updatedBook) {
+                    book.setAuthor(updatedBook.getAuthor());
+                } //If the item type is a Magazine, updates the Magazine attributes (edition)
+                else if (item instanceof Magazine mag && updatedItem instanceof Magazine updatedMag) {
+                    mag.setEdition(updatedMag.getEdition());
+                }
+                return item;
+            }
+        }
+
+        // Delete from catalog
+        public boolean deleteById(int id) {
+            return catalog.removeIf(item -> item.getId() == id);
+        }
+    }
+}
